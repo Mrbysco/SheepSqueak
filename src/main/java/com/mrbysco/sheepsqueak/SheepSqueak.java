76534538808f9f90f790bc6ea9c1416ddb3373e3
Mrbysco <Mrbysco@users.noreply.github.com>
2021-12-01 00:14:53 +0100

    Initial port to 1.18 [push]

--- src/main/java/com/mrbysco/sheepsqueak/SheepSqueak.java
+++ src/main/java/com/mrbysco/sheepsqueak/SheepSqueak.java
@@ -11,7 +11,7 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
