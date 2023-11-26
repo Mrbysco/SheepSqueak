320df2fcf79a781120be732814b72ab10dea43b9
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-26 21:33:58 +0100

    Port to 1.20.2 [build] [publish]

--- neoforge/src/main/java/com/mrbysco/sheepsqueak/SheepSqueakForge.java
+++ neoforge/src/main/java/com/mrbysco/sheepsqueak/SheepSqueakForge.java
@@ -1,9 +1,9 @@
 package com.mrbysco.sheepsqueak;
 
 import net.minecraft.world.entity.animal.Sheep;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
+import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
 public class SheepSqueakForge {
@@ -11,7 +11,7 @@ public class SheepSqueakForge {
 	public SheepSqueakForge() {
 		CommonClass.init();
 
-		MinecraftForge.EVENT_BUS.addListener(this::onLivingHurt);
+		NeoForge.EVENT_BUS.addListener(this::onLivingHurt);
 	}
 
 	private void onLivingHurt(LivingHurtEvent event) {
